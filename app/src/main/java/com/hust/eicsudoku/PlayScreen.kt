68bf7ecdea2418b7c8aa1e6f7a99cad8622f46b7
lfalive <542679373@qq.com>
2021-12-29 16:47:31 +0800

    clean code

--- app/src/main/java/com/hust/eicsudoku/PlayScreen.kt
+++ app/src/main/java/com/hust/eicsudoku/PlayScreen.kt
@@ -1,6 +1,5 @@
 package com.hust.eicsudoku
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.Typeface.BOLD
 import android.os.Bundle
@@ -9,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.RelativeLayout
+import androidx.gridlayout.widget.GridLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kotlin.collections.HashMap
@@ -19,12 +20,9 @@ class PlayScreen : Fragment(), View.OnClickListener {
     private var selectedButton: Button? = null
     private var userInputBoard = Array(9) { IntArray(9) { 0 } }
     private var matrix = Array(9) { IntArray(9) { 0 } }
-    private var inputButtonMap = HashMap<String, Button>()
     private var buttonMap = HashMap<String, Button>()
-    private var reverseMap = HashMap<Button, String>()
     private var timerVal: CountDownTimer? = null
     private val initialSolveTime = 3600000L
-    private var solvedCnt: Int = 55
     private lateinit var timer: TextView
     private lateinit var resetButton: Button
 
@@ -65,109 +63,25 @@ class PlayScreen : Fragment(), View.OnClickListener {
         requireActivity().onBackPressed()
         requireActivity().onBackPressed()
 
-        buttonMap["b11"] = view.findViewById(R.id.b11)
-        buttonMap["b12"] = view.findViewById(R.id.b12)
-        buttonMap["b13"] = view.findViewById(R.id.b13)
-        buttonMap["b14"] = view.findViewById(R.id.b14)
-        buttonMap["b15"] = view.findViewById(R.id.b15)
-        buttonMap["b16"] = view.findViewById(R.id.b16)
-        buttonMap["b17"] = view.findViewById(R.id.b17)
-        buttonMap["b18"] = view.findViewById(R.id.b18)
-        buttonMap["b19"] = view.findViewById(R.id.b19)
-        buttonMap["b21"] = view.findViewById(R.id.b21)
-        buttonMap["b22"] = view.findViewById(R.id.b22)
-        buttonMap["b23"] = view.findViewById(R.id.b23)
-        buttonMap["b24"] = view.findViewById(R.id.b24)
-        buttonMap["b25"] = view.findViewById(R.id.b25)
-        buttonMap["b26"] = view.findViewById(R.id.b26)
-        buttonMap["b27"] = view.findViewById(R.id.b27)
-        buttonMap["b28"] = view.findViewById(R.id.b28)
-        buttonMap["b29"] = view.findViewById(R.id.b29)
-        buttonMap["b31"] = view.findViewById(R.id.b31)
-        buttonMap["b32"] = view.findViewById(R.id.b32)
-        buttonMap["b33"] = view.findViewById(R.id.b33)
-        buttonMap["b34"] = view.findViewById(R.id.b34)
-        buttonMap["b35"] = view.findViewById(R.id.b35)
-        buttonMap["b36"] = view.findViewById(R.id.b36)
-        buttonMap["b37"] = view.findViewById(R.id.b37)
-        buttonMap["b38"] = view.findViewById(R.id.b38)
-        buttonMap["b39"] = view.findViewById(R.id.b39)
-        buttonMap["b41"] = view.findViewById(R.id.b41)
-        buttonMap["b42"] = view.findViewById(R.id.b42)
-        buttonMap["b43"] = view.findViewById(R.id.b43)
-        buttonMap["b44"] = view.findViewById(R.id.b44)
-        buttonMap["b45"] = view.findViewById(R.id.b45)
-        buttonMap["b46"] = view.findViewById(R.id.b46)
-        buttonMap["b47"] = view.findViewById(R.id.b47)
-        buttonMap["b48"] = view.findViewById(R.id.b48)
-        buttonMap["b49"] = view.findViewById(R.id.b49)
-        buttonMap["b51"] = view.findViewById(R.id.b51)
-        buttonMap["b52"] = view.findViewById(R.id.b52)
-        buttonMap["b53"] = view.findViewById(R.id.b53)
-        buttonMap["b54"] = view.findViewById(R.id.b54)
-        buttonMap["b55"] = view.findViewById(R.id.b55)
-        buttonMap["b56"] = view.findViewById(R.id.b56)
-        buttonMap["b57"] = view.findViewById(R.id.b57)
-        buttonMap["b58"] = view.findViewById(R.id.b58)
-        buttonMap["b59"] = view.findViewById(R.id.b59)
-        buttonMap["b61"] = view.findViewById(R.id.b61)
-        buttonMap["b62"] = view.findViewById(R.id.b62)
-        buttonMap["b63"] = view.findViewById(R.id.b63)
-        buttonMap["b64"] = view.findViewById(R.id.b64)
-        buttonMap["b65"] = view.findViewById(R.id.b65)
-        buttonMap["b66"] = view.findViewById(R.id.b66)
-        buttonMap["b67"] = view.findViewById(R.id.b67)
-        buttonMap["b68"] = view.findViewById(R.id.b68)
-        buttonMap["b69"] = view.findViewById(R.id.b69)
-        buttonMap["b71"] = view.findViewById(R.id.b71)
-        buttonMap["b72"] = view.findViewById(R.id.b72)
-        buttonMap["b73"] = view.findViewById(R.id.b73)
-        buttonMap["b74"] = view.findViewById(R.id.b74)
-        buttonMap["b75"] = view.findViewById(R.id.b75)
-        buttonMap["b76"] = view.findViewById(R.id.b76)
-        buttonMap["b77"] = view.findViewById(R.id.b77)
-        buttonMap["b78"] = view.findViewById(R.id.b78)
-        buttonMap["b79"] = view.findViewById(R.id.b79)
-        buttonMap["b81"] = view.findViewById(R.id.b81)
-        buttonMap["b82"] = view.findViewById(R.id.b82)
-        buttonMap["b83"] = view.findViewById(R.id.b83)
-        buttonMap["b84"] = view.findViewById(R.id.b84)
-        buttonMap["b85"] = view.findViewById(R.id.b85)
-        buttonMap["b86"] = view.findViewById(R.id.b86)
-        buttonMap["b87"] = view.findViewById(R.id.b87)
-        buttonMap["b88"] = view.findViewById(R.id.b88)
-        buttonMap["b89"] = view.findViewById(R.id.b89)
-        buttonMap["b91"] = view.findViewById(R.id.b91)
-        buttonMap["b92"] = view.findViewById(R.id.b92)
-        buttonMap["b93"] = view.findViewById(R.id.b93)
-        buttonMap["b94"] = view.findViewById(R.id.b94)
-        buttonMap["b95"] = view.findViewById(R.id.b95)
-        buttonMap["b96"] = view.findViewById(R.id.b96)
-        buttonMap["b97"] = view.findViewById(R.id.b97)
-        buttonMap["b98"] = view.findViewById(R.id.b98)
-        buttonMap["b99"] = view.findViewById(R.id.b99)
+        val sudokuMatrix: RelativeLayout = view.findViewById(R.id.sudokuMatrix)
+        for (i in 0 until sudokuMatrix.childCount) {
+            val matrixButton = sudokuMatrix.getChildAt(i)
+            val buttonString = matrixButton.toString()
+            val startInd = buttonString.lastIndexOf("app:id")
+            val subStr = buttonString.substring(startInd + 7, startInd + 10)
+            buttonMap[subStr] = matrixButton as Button
+        }
 
         generateMatrix()
-        for ((k, v) in buttonMap) {
-            reverseMap[v] = k
+        for ((_, v) in buttonMap) {
             if (v.isClickable)
                 v.setOnClickListener(this)
         }
 
-        inputButtonMap["1"] = view.findViewById(R.id.one)
-        inputButtonMap["2"] = view.findViewById(R.id.two)
-        inputButtonMap["3"] = view.findViewById(R.id.three)
-        inputButtonMap["4"] = view.findViewById(R.id.four)
-        inputButtonMap["5"] = view.findViewById(R.id.five)
-        inputButtonMap["6"] = view.findViewById(R.id.six)
-        inputButtonMap["7"] = view.findViewById(R.id.seven)
-        inputButtonMap["8"] = view.findViewById(R.id.eight)
-        inputButtonMap["9"] = view.findViewById(R.id.nine)
-        inputButtonMap["c"] = view.findViewById(R.id.clear)
-        for ((_, v) in inputButtonMap) {
-            v.setOnClickListener(this)
+        val inputButtons: GridLayout = view.findViewById(R.id.inputButtons)
+        for (i in 0 until inputButtons.childCount) {
+            inputButtons.getChildAt(i).setOnClickListener(this)
         }
-
         return view
     }
 
@@ -188,8 +102,7 @@ class PlayScreen : Fragment(), View.OnClickListener {
             R.id.clear -> {
                 selectedButton?.text = ""
                 if (selectedButton != null) {
-                    val row = reverseMap[selectedButton]?.get(1)?.toString()?.toInt()
-                    val col = reverseMap[selectedButton]?.get(2)?.toString()?.toInt()
+                    val (row, col) = getButtonPosition(selectedButton!!)
                     if (col != null && row != null) {
                         userInputBoard[row - 1][col - 1] = 0
                     }
@@ -209,8 +122,7 @@ class PlayScreen : Fragment(), View.OnClickListener {
                 val numButton: Button = requireView().findViewById(v.id)
                 selectedButton?.text = numButton.text.toString()
                 if (selectedButton != null) {
-                    val row = reverseMap[selectedButton]?.get(1)?.toString()?.toInt()
-                    val col = reverseMap[selectedButton]?.get(2)?.toString()?.toInt()
+                    val (row, col) = getButtonPosition(selectedButton!!)
                     if (col != null && row != null) {
                         userInputBoard[row - 1][col - 1] = numButton.text.toString().toInt()
                     }
@@ -231,21 +143,26 @@ class PlayScreen : Fragment(), View.OnClickListener {
         }
     }
 
+    //由Button控件对象获得Button坐标
+    private fun getButtonPosition(button: Button): Pair<Int?, Int?> {
+        val buttonString = button.toString()
+        val startInd = buttonString.lastIndexOf("app:id")
+        val subStr = buttonString.substring(startInd + 7, startInd + 10)
+        if (subStr[0] != 'b') return Pair(null, null)
+        return Pair(subStr[1].digitToIntOrNull(), subStr[2].digitToIntOrNull())
+    }
+
     //Highlight指选中的格子那一行和列里的其他可选格子
     private fun removeHighlight(button: Button) {
         if (selectedButton == null) return
 
-        val row = reverseMap[button]?.get(1)?.toString()?.toInt()
-        val col = reverseMap[button]?.get(2)?.toString()?.toInt()
-
-        for (i in 0 until 9) {
+        val (row, col) = getButtonPosition(button)
+        for (i in 0..8) {
             if (row != null) {
                 if (buttonMap["b${row}${i + 1}"]?.isClickable!!) {
                     buttonMap["b${row}${i + 1}"]?.setBackgroundResource(R.drawable.button_border)
                 }
             }
-        }
-        for (i in 0 until 9) {
             if (col != null) {
                 if (buttonMap["b${i + 1}${col}"]?.isClickable!!) {
                     buttonMap["b${i + 1}${col}"]?.setBackgroundResource(R.drawable.button_border)
@@ -255,17 +172,14 @@ class PlayScreen : Fragment(), View.OnClickListener {
     }
 
     private fun highlight(button: Button) {
-        val row = reverseMap[button]?.get(1)?.toString()?.toInt()
-        val col = reverseMap[button]?.get(2)?.toString()?.toInt()
+        val (row, col) = getButtonPosition(button)
 
-        for (i in 0 until 9) {
+        for (i in 0..8) {
             if (row != null) {
                 if (buttonMap["b${row}${i + 1}"]?.isClickable!!) {
                     buttonMap["b${row}${i + 1}"]?.setBackgroundResource(R.drawable.highlighted_button)
                 }
             }
-        }
-        for (i in 0 until 9) {
             if (col != null) {
                 if (buttonMap["b${i + 1}${col}"]?.isClickable!!) {
                     buttonMap["b${i + 1}${col}"]?.setBackgroundResource(R.drawable.highlighted_button)
@@ -274,14 +188,13 @@ class PlayScreen : Fragment(), View.OnClickListener {
         }
     }
 
-
     private fun resetBoard() {
         if (selectedButton == null) return
         removeHighlight(selectedButton!!)
         selectedButton!!.setBackgroundResource(R.drawable.button_border)
         selectedButton = null
-        for (i in 0 until 9) {
-            for (j in 0 until 9) {
+        for (i in 0..8) {
+            for (j in 0..8) {
                 if (buttonMap["b${i + 1}${j + 1}"]?.isClickable!!) {
                     buttonMap["b${i + 1}${j + 1}"]?.text = ""
                     userInputBoard[i][j] = 0
@@ -290,41 +203,29 @@ class PlayScreen : Fragment(), View.OnClickListener {
         }
     }
 
-    @SuppressLint("SetTextI18n")
     private fun check() {
+        val isRow = Array(9) { BooleanArray(9) { false } }
+        val isCol = Array(9) { BooleanArray(9) { false } }
+        val isSubMatrix = Array(3) { Array(3) { BooleanArray(9) { false } } }
         for (i in 0..8)
             for (j in 0..8) {
-                if (userInputBoard[i][j] == 0) {
-                    return
-                }
-                for (k in 0..8) {
-                    if (userInputBoard[i][j] == userInputBoard[i][k] && j != k) {
-                        return
-                    }
-                    if (userInputBoard[i][j] == userInputBoard[k][j] && i != k) {
-                        return
-                    }
-                }
+                if (userInputBoard[i][j] == 0) return
+                isRow[i][userInputBoard[i][j] - 1] = true
+                isCol[j][userInputBoard[i][j] - 1] = true
+                isSubMatrix[i / 3][j / 3][userInputBoard[i][j] - 1] = true
             }
-        for (a in 0..2)
-            for (b in 0..2) {
-                for (i in 0..2)
-                    for (j in 0..2) {
-                        val r = 3 * a
-                        val c = 3 * b
-                        for (x in 0..2)
-                            for (y in 0..2) {
-                                if (userInputBoard[i + r][j + c] == userInputBoard[x + r][y + c] && (x != i || y != j)) {
-                                    return
-                                }
-                            }
-                    }
-            }
+        for (i in 0..8)
+            for (k in 0..8)
+                if (!isRow[i][k] or !isCol[i][k]) return
+        for (i in 0..2)
+            for (j in 0..2)
+                for (k in 0..8)
+                    if (!isSubMatrix[i][j][k]) return
         KAlertDialog(activity, KAlertDialog.SUCCESS_TYPE)
             .setContentText("GREAT JOB, YOU SOLVED IT IN " + timer.text + " !")
             .show()
         timerVal!!.cancel()
-        resetButton.text = "BACK"
+        resetButton.text = getString(R.string.back)
     }
 
     //生成答案矩阵
@@ -339,9 +240,8 @@ class PlayScreen : Fragment(), View.OnClickListener {
             for (j in 0..8)
                 matrix[i][j] = randomMap[matrix[i][j] - 1]
         //根据所选难度设置题目中已知数字数量
-        val difficultyLevel = arguments?.getString("difficulty_text").toString()
-        solvedCnt = when (difficultyLevel) {
-            "easy" -> 80
+        var solvedCnt = when (arguments?.getString("difficulty_text").toString()) {
+            "easy" -> 78
             "medium" -> 42
             else -> 35
         }
@@ -365,19 +265,13 @@ class PlayScreen : Fragment(), View.OnClickListener {
 
     //(row,col)处填入num是否合法
     private fun isSafe(row: Int, col: Int, num: Int): Boolean {
-        for (d in 0 until 9) {
-            if (d == col)
-                continue
-            if (matrix[row][d] == num) {
-                return false
-            }
+        for (d in 0..8) {
+            if (d == col) continue
+            if (matrix[row][d] == num) return false
         }
-        for (r in 0 until 9) {
-            if (r == row)
-                continue
-            if (matrix[r][col] == num) {
-                return false
-            }
+        for (r in 0..8) {
+            if (r == row) continue
+            if (matrix[r][col] == num) return false
         }
 
         val sqrt = 3
@@ -386,10 +280,8 @@ class PlayScreen : Fragment(), View.OnClickListener {
 
         for (r in boxRowStart until (boxRowStart + sqrt)) {
             for (d in boxColStart until (boxColStart + sqrt)) {
-                if (d == col && r == row)
-                    continue
-                if (matrix[r][d] == num)
-                    return false
+                if (d == col && r == row) continue
+                if (matrix[r][d] == num) return false
             }
         }
         return true
